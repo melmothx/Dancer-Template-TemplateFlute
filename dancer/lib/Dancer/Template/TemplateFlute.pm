e67ff211b29bc2ed0561d3b939a27b4b68137924
Stefan Hornburg (Racke) <racke@linuxia.de>
2014-03-09 14:04:35 +0100

    Bump up version number.

--- dancer/lib/Dancer/Template/TemplateFlute.pm
+++ dancer/lib/Dancer/Template/TemplateFlute.pm
@@ -13,7 +13,7 @@ use Dancer::Config;
 
 use base 'Dancer::Template::Abstract';
 
-our $VERSION = '0.0104';
+our $VERSION = '0.0106';
 
 =head1 NAME
 
@@ -21,7 +21,7 @@ Dancer::Template::TemplateFlute - Template::Flute wrapper for Dancer
 
 =head1 VERSION
 
-Version 0.0104
+Version 0.0106
 
 =head1 DESCRIPTION
 
